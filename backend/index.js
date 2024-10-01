b733438957dc173f058c98db3f40989da1ad74b0
Nikolas Manuel <nikolas.manuel13@gmail.com>
2024-10-01 18:03:34 -0500

    first commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const PORT = process.env.port || 4000;
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
